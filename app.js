d1d4021598dc441a8b63466cde2e852f72140402
DonMGameR <donmoutsystems.js@gmail.com>
2023-01-31 10:44:41 -0300

    Refatorando codigo e finalizando estudos em promises

--- app.js
+++ app.js
@@ -31,16 +31,22 @@
 const url = 'https://dog.ceo/api/breeds/image/random'
 const dogImg = document.querySelector('[data-js="dog-img"]')
 
+const validateHTTPStatus = dogData => {
+    if (!dogData.ok) {
+        throw new Error(`HTTP error, status ${dogData.status}`)
+    }
+    return dogData.json()
+}
+
+const setDogImage = ({ message: url }) => {
+    dogImg.setAttribute('src', url)
+}
+
+const handleRequestError = error => {
+    console.log(error.message)
+}
+
 fetch(url)
-    .then(dogData => {
-        if (!dogData.ok) {
-            throw new Error(`HTTP error, status ${dogData.status}`)
-        }
-        return dogData.json()
-    })
-    .then(({ message }) => {
-        dogImg.setAttribute('src', message)
-    })
-    .catch(error => {
-        console.log(error.message)
-    })
+    .then(validateHTTPStatus)
+    .then(setDogImage)
+    .catch(handleRequestError)
